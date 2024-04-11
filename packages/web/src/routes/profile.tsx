d2890ca08a635b4bf575960737da64296984f2ee
Jashan Sran <jashanpreet.sran@gmail.com>
2024-04-10 18:38:29 -0700

    ch

--- packages/web/src/routes/profile.tsx
+++ packages/web/src/routes/profile.tsx
@@ -1,9 +1,5 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute("/profile")({
-  component: Profile,
-});
-
-function Profile() {
-  return <div className="p-2">Hello from Profile!</div>;
-}
+export const Route = createFileRoute('/profile')({
+  component: () => <div>Hello /profile!</div>
+})
