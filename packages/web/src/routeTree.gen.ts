ce749c5053ce9a4ad96415c6ffba04f7cc3b25c7
Jashan Sran <jashanpreet.sran@gmail.com>
2024-04-10 23:55:59 -0700

    updated

--- packages/web/src/routeTree.gen.ts
+++ packages/web/src/routeTree.gen.ts
@@ -10,64 +10,64 @@
 
 // Import Routes
 
-import { Route as rootRoute } from "./routes/__root";
-import { Route as AboutImport } from "./routes/about";
-import { Route as AuthenticatedImport } from "./routes/_authenticated";
-import { Route as AuthenticatedIndexImport } from "./routes/_authenticated/index";
-import { Route as AuthenticatedProfileImport } from "./routes/_authenticated/profile";
-import { Route as AuthenticatedCreateImport } from "./routes/_authenticated/create";
+import { Route as rootRoute } from './routes/__root'
+import { Route as AboutImport } from './routes/about'
+import { Route as AuthenticatedImport } from './routes/_authenticated'
+import { Route as AuthenticatedIndexImport } from './routes/_authenticated/index'
+import { Route as AuthenticatedProfileImport } from './routes/_authenticated/profile'
+import { Route as AuthenticatedCreateImport } from './routes/_authenticated/create'
 
 // Create/Update Routes
 
 const AboutRoute = AboutImport.update({
-  path: "/about",
+  path: '/about',
   getParentRoute: () => rootRoute,
-} as any);
+} as any)
 
 const AuthenticatedRoute = AuthenticatedImport.update({
-  id: "/_authenticated",
+  id: '/_authenticated',
   getParentRoute: () => rootRoute,
-} as any);
+} as any)
 
 const AuthenticatedIndexRoute = AuthenticatedIndexImport.update({
-  path: "/",
+  path: '/',
   getParentRoute: () => AuthenticatedRoute,
-} as any);
+} as any)
 
 const AuthenticatedProfileRoute = AuthenticatedProfileImport.update({
-  path: "/profile",
+  path: '/profile',
   getParentRoute: () => AuthenticatedRoute,
-} as any);
+} as any)
 
 const AuthenticatedCreateRoute = AuthenticatedCreateImport.update({
-  path: "/create",
+  path: '/create',
   getParentRoute: () => AuthenticatedRoute,
-} as any);
+} as any)
 
 // Populate the FileRoutesByPath interface
 
-declare module "@tanstack/react-router" {
+declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
-    "/_authenticated": {
-      preLoaderRoute: typeof AuthenticatedImport;
-      parentRoute: typeof rootRoute;
-    };
-    "/about": {
-      preLoaderRoute: typeof AboutImport;
-      parentRoute: typeof rootRoute;
-    };
-    "/_authenticated/create": {
-      preLoaderRoute: typeof AuthenticatedCreateImport;
-      parentRoute: typeof AuthenticatedImport;
-    };
-    "/_authenticated/profile": {
-      preLoaderRoute: typeof AuthenticatedProfileImport;
-      parentRoute: typeof AuthenticatedImport;
-    };
-    "/_authenticated/": {
-      preLoaderRoute: typeof AuthenticatedIndexImport;
-      parentRoute: typeof AuthenticatedImport;
-    };
+    '/_authenticated': {
+      preLoaderRoute: typeof AuthenticatedImport
+      parentRoute: typeof rootRoute
+    }
+    '/about': {
+      preLoaderRoute: typeof AboutImport
+      parentRoute: typeof rootRoute
+    }
+    '/_authenticated/create': {
+      preLoaderRoute: typeof AuthenticatedCreateImport
+      parentRoute: typeof AuthenticatedImport
+    }
+    '/_authenticated/profile': {
+      preLoaderRoute: typeof AuthenticatedProfileImport
+      parentRoute: typeof AuthenticatedImport
+    }
+    '/_authenticated/': {
+      preLoaderRoute: typeof AuthenticatedIndexImport
+      parentRoute: typeof AuthenticatedImport
+    }
   }
 }
 
@@ -80,6 +80,6 @@ export const routeTree = rootRoute.addChildren([
     AuthenticatedIndexRoute,
   ]),
   AboutRoute,
-]);
+])
 
 /* prettier-ignore-end */
